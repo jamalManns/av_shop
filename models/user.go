077f6dc72aa9274075288573c9e0df96e10ee0b8
jamalManns <199225111+jamalManns@users.noreply.github.com>
2025-02-16 15:28:41 +0300

    add auth and error structs

--- models/user.go
+++ models/user.go
@@ -5,3 +5,18 @@ type User struct {
 	Username string `json:"username"`
 	Balance  int    `json:"balance"`
 }
+
+// AuthRequest содержит данные для аутентификации
+type AuthRequest struct {
+	Username string `json:"username" binding:"required"`
+}
+
+// ErrorResponse используется для ответов об ошибках
+type ErrorResponse struct {
+	Errors string `json:"errors"`
+}
+
+// AuthResponse содержит токен после успешной аутентификации
+type AuthResponse struct {
+	Token string `json:"token"`
+}
